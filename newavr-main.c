bde160e86e085129cbad94462cbb0f6c72ac94a0
Enrique Vázquez <83434908+Quique2000@users.noreply.github.com>
2021-05-03 23:27:54 -0500

    Add files via upload

---
+++ newavr-main.c
@@ -0,0 +1,15 @@
+#define F_CPU 10000000UL 
+#include <xc.h> 
+#include <util/delay.h> 
+#include <avr/io.h> 
+
+int main(void){
+    DDRB=0x01; 
+    PORTB=0x01;
+    while (1)     {
+        PORTB=0b00000001; 
+        _delay_ms(2000);
+        PORTB=0b00000001;
+        _delay_ms(2000);
+    }  
+}  
